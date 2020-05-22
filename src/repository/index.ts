7413100533ad7ce99736b7161f05de01f4a24ca0
EntropicFrost <brownjacob998@gmail.com>
2020-05-22 16:15:27 -0400

    jenkins

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg'
 
 export const connectionPool : Pool = new Pool({
     host: process.env['PG_HOST'],
-    user: process.env['Pg_USER'],
+    user: process.env['PG_USER'],
     password: process.env['PG_PASSWORD'],
     database: process.env['PG_DATABASE'],
     port: 5432,
